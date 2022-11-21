a6a24162d1c94698d8410f711a908cf3f19ddc5c
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-11-21 18:24:22 -0500

    wip: Cleans up code.

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 import random
 
 user_cards = []
 computer_cards = []
-
-# def draw_card():
-#   random.randrange(1,13) - 1
-
-# def deal_card():
-#   draw = random.randrange(1,13) - 1
-#   user_count = len(user_cards)
-#   if  user_count < 2:
-#     user_cards.append(cards[draw])
-#     deal_card()
 
 def deal_card():
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -25,8 +14,8 @@ def deal_card():
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 
 for _ in range(2):
-  new_card = deal_card()
-  user_cards.append(new_card)
+  user_cards.append(deal_card())
+  computer_cards.append(deal_card())
   
 print(user_cards)
 

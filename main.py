2a9b075fd92d8a173a9052664209f2300f3654d3
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-11-22 10:25:39 -0500

    wip: Refactors deal_again function.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 def deal_card():
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -38,23 +39,27 @@ def calculate_score(cards):
 
 #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
+while not is_game_over:
 
-print(f"Your cards: {user_cards} totaling {user_score}.")
-print(f"Dealers card: {computer_cards[0]}")
+  user_score = calculate_score(user_cards)
+  computer_score = calculate_score(computer_cards)
 
-is_game_over = False
+  print(f"Your cards: {user_cards} totaling {user_score}.")
+  print(f"Dealers card: {computer_cards[0]}")
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-  is_game_over = True
+  # is_game_over = False
+
+  if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
 
 #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
 
-else: 
-  deal_again = input("Input Y to be delt another card or N to stay: ").lower
-  if deal_again == "y":
-    user_cards.append(deal_card())
+  else: 
+    deal_again = input("Input y to be delt another card or n to stay: ")
+    if deal_again == "y":
+      user_cards.append(deal_card())
+    else:
+      is_game_over = True
 
 #Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
 
